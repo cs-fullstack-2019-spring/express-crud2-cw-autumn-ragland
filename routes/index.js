fa99bfeab0ed8dca713f07684663f856fa3b4fc4
autumn-ragland <raglanda998@gmail.com>
2019-03-26 15:09:18 -0500

    cw push

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var PostCollection = require('../models/PostSchema');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// list all posts
+router.get('/', (req, res) => {
+    PostCollection.find({}, (error, results) => {
+        if (error) res.send(error);
+        else res.render('index', {posts: results});
+    });
+});
+
+//render create form
+router.get('/create', (req, res) => {
+    res.render('create');
+});
+
+// create a post
+router.get('/createAction', (req, res) => {
+    PostCollection.create({
+        userId: req.query.userId,
+        id: req.query.id,
+        title: req.query.title,
+        body: req.query.body
+    }, (error, results) => {
+        if (error) res.send(error);
+        else res.redirect('/');
+    });
+});
+
+//render update form
+router.get('/update', (req, res) => {
+    res.render('update');
+});
+
+//update existing post
+router.get('/updateAction', (req, res) => {
+    PostCollection.findOneAndUpdate(
+        {id: req.query.id},
+        {
+            userId: req.query.userId,
+            title: req.query.title,
+            body: req.query.body
+        }, (error) => {
+            if (error) res.send(error);
+            else res.redirect('/')
+        })
+});
+
+//render delete form
+router.get('/delete', (req, res) => {
+    res.render('delete');
+});
+
+// delete post
+router.get('/deleteAction', (req, res) => {
+    PostCollection.deleteOne({id:req.query.id}, (error) => {
+        if (error) res.send(error);
+        else res.redirect('/');
+    });
 });
 
 module.exports = router;
